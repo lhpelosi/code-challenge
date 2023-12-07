5df5afb17ea08bc8c254210c8b3de3cc861013ea
lhpelosi <lhpelosi@gmail.com>
2023-12-07 13:06:14 -0300

    Aoc23 day 7

---
+++ aoc2023/day07/q1.py
@@ -0,0 +1,47 @@
+# Order hands of cards
+
+import sys
+from enum import Enum
+from itertools import groupby
+
+LABEL_ORDER = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+class HandType(Enum):
+    FIVE = 1
+    FOUR = 2
+    FULL_HOUSE = 3
+    THREE = 4
+    TWO_PAIR = 5
+    ONE_PAIR = 6
+    SINGLE = 7
+
+def identify_type(hand_string):
+    sorted_groups_n = sorted([len(list(label_iter)) for _, label_iter in groupby(sorted(hand_string))], reverse=True)
+    if sorted_groups_n[0] == 5:
+        return HandType.FIVE
+    if sorted_groups_n[0] == 4:
+        return HandType.FOUR
+    if sorted_groups_n[0] == 3:
+        if sorted_groups_n[1] == 2:
+            return HandType.FULL_HOUSE
+        return HandType.THREE
+    if sorted_groups_n[0] == 2:
+        if sorted_groups_n[1] == 2:
+            return HandType.TWO_PAIR
+        return HandType.ONE_PAIR
+    return HandType.SINGLE
+
+
+hand_list = []
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        l = line.split()
+        hand_list.append((l[0], identify_type(l[0]), int(l[1])))
+
+def sort_hand_key(hand):
+    hand_string, hand_type, bid = hand
+    return (hand_type.value, [LABEL_ORDER.index(card) for card in hand_string])
+sorted_hand_list = sorted(hand_list, key=sort_hand_key, reverse=True)
+
+sorted_bid_list = [bid for _, _, bid in sorted_hand_list]
+total = sum([(i+1)*bid for i, bid in enumerate(sorted_bid_list)])
+print(total)
